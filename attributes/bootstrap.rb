80a4e5780524c21e68e602cf4e99d78dc0dc29b8
Michael Huynh <mike.huynh@gmail.com>
2014-06-15 17:13:39 -0400

    Bumped to v0.2.1

--- attributes/bootstrap.rb
+++ attributes/bootstrap.rb
@@ -1,12 +1,12 @@
 default['dokku']['git_repository'] = 'https://github.com/progrium/dokku.git'
-default['dokku']['git_revision'] = 'v0.2.0'
+default['dokku']['git_revision'] = 'v0.2.1'
 
 default['dokku']['pluginhook']['src_url'] = 'https://s3.amazonaws.com/progrium-pluginhook/pluginhook_0.1.0_amd64.deb'
 default['dokku']['pluginhook']['filename'] = 'pluginhook_0.1.0_amd64.deb'
 default['dokku']['pluginhook']['checksum'] = '26a790070ee0c34fd4c53b24aabeb92778faed4004110c480c13b48608545fe5'
 
 default['dokku']['sshcommand']['filename'] = 'sshcommand'
-default['dokku']['sshcommand']['src_url'] = 'https://raw.github.com/progrium/sshcommand/master/sshcommand'
+default['dokku']['sshcommand']['src_url'] = 'https://raw.github.com/progrium/sshcommand/5f9afe79698332d24a69873721619f5af4670d09/sshcommand'
 
 default['dokku']['docker']['use_aufs'] = true
 
