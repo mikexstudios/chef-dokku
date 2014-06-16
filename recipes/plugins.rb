f0bb4af5388ad2e97c4a7e20545f9c6568eb6e28
Michael Huynh <mike.huynh@gmail.com>
2014-06-16 11:46:02 -0400

    Added ability to specify plugin revision.

--- recipes/plugins.rb
+++ recipes/plugins.rb
@@ -1,6 +1,7 @@
-node['dokku']['plugins'].each do |plugin_name, repo_url|
-  git "#{node['dokku']['plugin_path']}/#{plugin_name}" do
-    repository repo_url
+node['dokku']['plugins'].each do |name, settings|
+  git "#{node['dokku']['plugin_path']}/#{name}" do
+    repository settings['repository']
+    revision settings['revision']
     action :sync
   end
 end
